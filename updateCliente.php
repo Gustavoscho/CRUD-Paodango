1ec770b1fed7aece4e3eec79066e8658f6d2dcaf
Caio <caio_ponick@estudante.sesisenai.org.br>
2025-08-15 11:37:48 -0300

    add

---
+++ updateCliente.php
@@ -0,0 +1,60 @@
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<?php
+
+include 'db.php';
+
+$id = $_GET['id'];
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $name = $_POST['nome'];
+    $email = $_POST['email'];
+
+    $sql = "UPDATE clientes SET name ='$name',email ='$email' WHERE id=$id";
+
+    if ($conn->query($sql) === true) {
+        echo "Registro atualizado com sucesso.
+        <a href='readCliente.php'>Ver registros.</a>
+        ";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+    exit(); 
+}
+
+$sql = "SELECT * FROM clientes WHERE id=$id";
+$result = $conn -> query($sql);
+$row = $result -> fetch_assoc();
+
+
+?>
+    <form method="POST" action="update.php?id=<?php echo $row['id'];?>">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="nome" value="<?php echo $row['nome'];?>" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" value="<?php echo $row['email'];?>" required>
+
+        <input type="submit" value="Atualizar">
+
+    </form>
+
+    <a href="readCliente.php">Ver registros.</a>
+
+</body>
+
+</html>
